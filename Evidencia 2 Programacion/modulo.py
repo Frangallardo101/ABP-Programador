f82e4b76a2706d58bffe17746e18063cf5ef9047
Francisco Gallardo <frangallardo101@gmail.com>
2025-05-12 18:00:00 -0300

    Segundo guardado, funciones arregladas

---
+++ Evidencia 2 Programacion/modulo.py
@@ -0,0 +1,111 @@
+def controlar_dispositivos(lista_dispositivos):
+    
+    while True: 
+        
+        print("A) Agregar un dispositivo \n B) Listar dispositivos \n C) Buscar dispositivo \n D) Eliminar un dispositivo")
+        
+        opcion = input("Seleccione una opcion: ")
+        
+        if opcion.capitalize() == "A":
+            
+            Agregar_dispositivos(lista_dispositivos)
+            
+        if opcion.capitalize() == "B":
+            
+            listar_dispositivos(lista_dispositivos)
+        
+        if opcion.capitalize() == "C":
+            
+            buscar_dispositivo(lista_dispositivos)
+        
+        if opcion.capitalize() == "D":
+            
+            eliminar_dispositivo(lista_dispositivos)
+            
+        if opcion.capitalize() == "E":
+            
+            break
+            
+
+def Agregar_dispositivos(lista_dispositivos):
+    
+    dispositivo = {}
+    
+    dispositivo['nombre'] = input("Cual es el dispositivo a agregar?: ")
+    
+    dispositivo['estado'] = 'apagado'
+       
+    while True :
+        
+        accion = input("Que accion realiza el dispositivo?: ")
+        
+        dispositivo[accion] = 'desactivada'
+        
+        if (input("El dispositivo realiza otra accion?: ")).lower() == 'si':
+            
+            continue
+        
+        elif accion.lower() == "no":
+            
+            break
+        
+        else: 
+            
+            accion = input("Ingrese una accion valida: ")
+    
+    lista_dispositivos.append(dispositivo)
+    
+def listar_dispositivos(lista_dispositivos):
+    
+    for i in range(0,len(lista_dispositivos)):
+        
+        print((lista_dispositivos[i])['nombre'])
+        
+def buscar_dispositivo(lista_dispositivos): 
+    
+    dispositivo = input("Cual es el dispositivo a buscar?: ")
+    
+    while True:
+        
+        for i in lista_dispositivos:
+            
+            if dispositivo in i.items():
+                
+                print(i)
+            
+            else:
+                 
+                continue
+            
+def eliminar_dispositivo(lista_dispositivos):
+    
+    dispositivo = input("Cual es el dispositivo a eliminar?: ")
+    
+    while True:
+        
+        for i in lista_dispositivos:
+            
+            for key,value in i.items():
+                
+                if value == dispositivo:
+                    
+                    lista_dispositivos.remove(i)
+                    
+                    print("El dispositivo fue eliminado")
+                
+                break
+            
+            else:
+                 
+                continue
+        
+        print("El dispositivo no fue encontrado")
+        
+        break
+    
+    
+            
+    
+            
+            
+            
